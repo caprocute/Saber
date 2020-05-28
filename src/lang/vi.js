de59b9ab3441b7eaebceefef7099174890734035
caprocute <hoangkhachieu96@gmail.com>
2020-05-28 14:34:12 +0700

    [RELEASE][28052020] INIT SOURCE CODE FROM SABER

---
+++ src/lang/vi.js
@@ -0,0 +1,102 @@
+export default {
+  title: 'Hệ thống demo',
+  tip: 'tip',
+  logoutTip: 'Bạn có muốn thoát ?',
+  submitText: 'Gửi',
+  cancelText: 'Hủy',
+  search: 'Vui lòng nhập nội dung tìm kiếm',
+  menuTip: 'Danh sách trống',
+  wel: {
+    info: 'Good morning, Smallwei, Avuex is a framework',
+    dept: 'a certain technology department',
+    team: 'Team ranking',
+    project: 'Project access',
+    count: 'Item number',
+    data: {
+      subtitle: 'real time',
+      column1: 'Classified statistics',
+      column2: 'Annex statistics',
+      column3: 'Article statistics',
+      key1: 'C',
+      key2: 'A',
+      key3: 'A',
+      text1: 'Total Record Number of Classifications',
+      text2: 'Number of attachments Uploaded',
+      text3: 'Comment frequency'
+    },
+    data2: {
+      column1: 'Registration today',
+      column2: 'Login today',
+      column3: 'Subscription today',
+      column4: 'Todays review'
+    },
+    data3: {
+      column1: 'Conversion rate（Day 28%）',
+      column2: 'Attendance rate（Day 11%）',
+      column3: 'Attendance rate（Day 33%）'
+    },
+    data4: {
+      column1: 'Error log',
+      column2: 'Data display',
+      column3: 'Privilege management',
+      column4: 'user management'
+    },
+    table: {
+      rw: 'Work Tasks',
+      nr: 'Work content',
+      sj: 'Working hours',
+    }
+  },
+  route: {
+    info: 'info',
+    website: 'website',
+    avuexwebsite: 'avuex',
+    dashboard: 'dashboard',
+    tags: 'tags',
+    store: 'store',
+    permission: 'permission',
+    api: 'api',
+    logs: 'logs',
+    table: 'table',
+    form: 'form',
+    data: 'data',
+    error: 'error',
+    test: 'test'
+  },
+  login: {
+    title: 'Đăng nhập ',
+    info: 'Xin chào',
+    tenantId: 'Vui lòng nhập tenantID',
+    username: 'Vui lòng nhập tên đăng nhập',
+    password: 'Vui lòng nhập mật khẩu',
+    wechat: 'Wechat',
+    qq: 'QQ',
+    phone: 'Vui lòng nhập số điện thoại của bạn',
+    code: 'Vui lòng nhập mã xác thực',
+    submit: 'Đăng nhập',
+    userLogin: 'Xác thực tài khoản',
+    phoneLogin: 'Xác thực số điện thoại',
+    thirdLogin: 'Phương thức khác',
+    msgText: 'Gửi mã',
+    msgSuccess: 'Thành công',
+  },
+  navbar: {
+    info: 'info',
+    logOut: 'logout',
+    userinfo: 'userinfo',
+    dashboard: 'dashboard',
+    lock: 'lock',
+    bug: 'none bug',
+    bugs: 'bug',
+    screenfullF: 'exit screenfull',
+    screenfull: 'screenfull',
+    language: 'language',
+    theme: 'theme',
+    color: 'color'
+  },
+  tagsView: {
+    menu: 'menu',
+    closeOthers: 'Close Others',
+    closeAll: 'Close All'
+  }
+}
